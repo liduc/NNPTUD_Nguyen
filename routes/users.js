05e8f45aaed1ae462d449fb6fc6ec2115ccec5c3
liduc <liduc1234@gmail.com>
2025-03-31 10:31:34 +0700

    [ADD bài tập]

--- routes/users.js
+++ routes/users.js
@@ -1,67 +1,62 @@
 var express = require('express');
-const { token } = require('morgan');
 var router = express.Router();
-var userControllers = require('../controllers/users')
-let jwt = require('jsonwebtoken');
-let { check_authentication, check_authorization } = require("../utils/check_auth");
+var userControllers = require('../controllers/users');
+const { check_authentication, check_authorization } = require("../utils/check_auth");
 const constants = require('../utils/constants');
 
-/* GET users listing. */
-router.get('/', check_authentication, check_authorization(['admin'])
-  , async function (req, res, next) {
-    try {
-      let users = await userControllers.getAllUsers()
-      res.send({
-        success: true,
-        data: users
-      });
-    } catch (error) {
-      next(error)
+// GET all: Yêu cầu mod
+router.get('/', check_authentication, check_authorization(constants.MOD_PERMISSION), async function (req, res, next) {
+  try {
+    let users = await userControllers.getAllUsers();
+    res.send({ success: true, data: users });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET by ID: Yêu cầu mod, trừ chính user
+router.get('/:id', check_authentication, check_authorization(constants.MOD_PERMISSION), async function (req, res, next) {
+  try {
+    if (req.user.id === req.params.id) {
+      return res.status(403).send({ success: false, message: "Không thể xem thông tin của chính bạn qua route này" });
     }
-  });
+    let user = await userControllers.getUserById(req.params.id);
+    if (!user) throw new Error("User not found");
+    res.send({ success: true, data: user });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST: Yêu cầu admin
 router.post('/', check_authentication, check_authorization(constants.ADMIN_PERMISSION), async function (req, res, next) {
   try {
     let body = req.body;
-    let newUser = await userControllers.createAnUser(
-      body.username,
-      body.password,
-      body.email,
-      body.role
-    )
-    res.status(200).send({
-      success: true,
-      message: newUser
-    });
+    let newUser = await userControllers.createAnUser(body.username, body.password, body.email, body.role);
+    res.status(200).send({ success: true, data: newUser });
   } catch (error) {
-    res.status(404).send({
-      success: false,
-      message: error.message
-    });
+    next(error);
   }
+});
 
-});
-router.put('/:id', async function (req, res, next) {
+// PUT: Yêu cầu admin
+router.put('/:id', check_authentication, check_authorization(constants.ADMIN_PERMISSION), async function (req, res, next) {
   try {
-    let body = req.body;
-    let updatedUser = await userControllers.updateAnUser(req.params.id, body);
-    res.status(200).send({
-      success: true,
-      message: updatedUser
-    });
+    let updatedUser = await userControllers.updateAnUser(req.params.id, req.body);
+    res.status(200).send({ success: true, data: updatedUser });
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
-router.delete('/:id', async function (req, res, next) {
+
+// DELETE: Yêu cầu admin
+router.delete('/:id', check_authentication, check_authorization(constants.ADMIN_PERMISSION), async function (req, res, next) {
   try {
     let deleteUser = await userControllers.deleteAnUser(req.params.id);
-    res.status(200).send({
-      success: true,
-      message: deleteUser
-    });
+    res.status(200).send({ success: true, data: deleteUser });
   } catch (error) {
-    next(error)
+    next(error);
   }
-
 });
+
 module.exports = router;
